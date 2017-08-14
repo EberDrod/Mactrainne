d341c7a1015046983db8cbd68f3133a764561b76
EP <droid@MacBook-Pro-de-EP.local>
2017-08-14 12:34:37 -0700

    Se Hicieron ejemploes de 1ra.App en IOS

---
+++ calculadora.playground/Contents.swift
@@ -0,0 +1,29 @@
+
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+var str = "Hello, playground" //Implicita -
+
+
+
+var str2:String = "Hola mundo" //Explicita
+
+
+
+
+
+///Variable nula
+
+var  Optional: String? = nil
+
+let  Const = "32"
+
+
+
+
+
+
